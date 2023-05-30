78d5a46fc1fffb14e139047030768fc528bd81e6
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-29 19:34:21 -0600

    primera version

---
+++ Pilas.py
@@ -0,0 +1,18 @@
+#Pilas
+
+pila = [1,2,3]
+
+#Agregando elemenetos por el final de la lista
+pila.append(4)
+pila.append(5)
+print(pila)
+
+#Eliminando último elemento de la lista
+n = pila.pop()
+print(f"Eliminando el elemento: {n}")
+n = pila.pop()
+print(f"Eliminando el elemento: {n}")
+
+print(pila)
+
+#Carolina EM
